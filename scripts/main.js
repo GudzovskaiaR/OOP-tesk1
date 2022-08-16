0e6c571a5303bc78b02ff8e90f214d3dc24e672b
GudzovskaiaR <100199861+GudzovskaiaR@users.noreply.github.com>
2022-08-16 10:51:40 +0300

    did 5 tesks

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,43 @@
 // Create a dog object with name and numLegs properties, and set them to a string and a number, respectively.
-// let dog = {
-//     name: 'Marta',
-//     numLegs: 4
-//     };
+let dog = {
+  name: "Marta",
+  numLegs: 4,
+};
 
-    // Print both properties of the dog object to your console.
-    // let dog = {
-    //     name: "Spot",
-    //     numLegs: 4
-    //   };
-    //   console.log(`Name: ${dog.name}, numLegs:${dog.numLegs}`)
+// Print both properties of the dog object to your console.
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+};
+console.log(`Name: ${dog.name}, numLegs:${dog.numLegs}`);
 
+// Using the dog object, give it a method called sayLegs. The method should return the sentence This dog has 4 legs.
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function () {
+    return "This dog has " + dog.numLegs + " legs.";
+  },
+};
 
-      // Using the dog object, give it a method called sayLegs. The method should return the sentence This dog has 4 legs.
-      // let dog = {
-      //   name: "Spot",
-      //   numLegs: 4,
-      //   sayLegs: function() {return "This dog has " + dog.numLegs + " legs.";}
-      
-      // };
-      
-      // dog.sayLegs();
+dog.sayLegs();
 
-      // Modify the dog.sayLegs method to remove any references to dog. Use the duck example for guidance
+// Modify the dog.sayLegs method to remove any references to dog. Use the duck example for guidance
 
-      // let dog = {
-      //   name: "Spot",
-      //   numLegs: 4,
-      //   sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
-      // };
-      
-      // dog.sayLegs();
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function () {
+    return "This dog has " + this.numLegs + " legs.";
+  },
+};
 
-      // Create a constructor, Dog, with properties name, color, and numLegs that are set to a string, a string, and a number, respectively.
+dog.sayLegs();
 
-      function Dog(){
-        this.name = "Marta";
-        this.color = "ginger";
-        this.numLegs = 4;
-      }
+// Create a constructor, Dog, with properties name, color, and numLegs that are set to a string, a string, and a number, respectively.
+
+function Dog() {
+  this.name = "Marta";
+  this.color = "ginger";
+  this.numLegs = 4;
+}
